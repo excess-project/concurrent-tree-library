2f1bb71e95b07786b7523052493182f990a6458d
Ibrahim Umar <ibrahim.umar@uit.no>
2014-05-09 17:43:46 -0400

    Initial Commit

---
+++ NewCBTree/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  NewCBTree
+//
+//  Created by Ibrahim Umar on 09/05/14.
+//  Copyright (c) 2014 Ibrahim Umar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
